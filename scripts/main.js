37217e8d8bebaebcb429568edcf7ea86fe6fa7f7
sushil24sk <kiskusushil24@gmail.com>
2020-04-10 01:56:16 +0530

    Few Changes to button.

--- scripts/main.js
+++ scripts/main.js
@@ -8,3 +8,27 @@ myImage.onclick = function() {
       myImage.setAttribute ('src','images/L1.png');
     }
 }
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+    let myName = prompt('Please enter your name.');
+    if(!myName || myName === null) {
+      setUserName();
+    } else {
+      localStorage.setItem('name', myName);
+      myHeading.innerHTML = 'Labradors are cute, ' + myName;
+    }
+  }
+
+  if(!localStorage.getItem('name')) {
+    setUserName();
+  } else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Labradors are cute, ' + storedName;
+  }
+
+  myButton.onclick = function() {
+    setUserName();
+  }
